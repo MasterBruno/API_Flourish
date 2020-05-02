57c707bf9a9c08ea4aec27ec9c6e195cb923c0ac
Bruno César <brunoc.15@hotmail.com>
2020-05-02 10:31:59 -0300

    Reestruturação da pastas do projeto.

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Home!')
+    res.send('Rota Usuários!')
 });
 
-module.exports = router;
+module.exports = router;  
