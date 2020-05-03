d6591b54c8e56099dd4d494c826940ffadc1911f
guilherme <gleite98@gmail.com>
2020-05-03 20:56:55 -0300

    Finalizando todas as rotas

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,29 +1,89 @@
 var express = require('express');
+const app = express();  
+
 var router = express.Router();
 
-/* GET Listagem posts. */
+
+/* GET Listagem status_dream. */
+/*
 router.get('/', function(req, res, next) {
-    res.send('Rota posts!')
+    res.send('Rota status_dream!')
 });
-
-/* GET Busca posts por id. */
+*/
+/* GET Busca status_dream por id. */
+/*
 router.get('/:id', function(req, res, next) {
-    res.send('Rota posts!')
+    res.send('Rota status_dream!')
 });
 
-/* POST Cadastro de posts. */
-router.post('/', function(req, res, next) {
-    res.send('Rota posts!')
+*/
+
+
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+function execSQLQuery(sqlQry, res){
+    const connection = mysql.createConnection({
+        host     : 'localhost',
+        port     : 3306,
+        user     : 'root',
+        password : 'teste123',
+        database : 'SistemaBD'
+    });
+
+    connection.query(sqlQry, function(error, results, fields){
+        if(error) 
+            res.json(error);
+        else
+            res.json(results);
+        connection.end();
+        console.log('executou!');
+    });
+}
+
+//Show all posts
+router.get('/', (req, res) => {
+	execSQLQuery('SELECT * FROM post', res);
 });
 
-/* PUT Atualiza todos os campos do posts. */
-router.put('/:id', function(req, res, next) {
-    res.send('Rota posts!')
+//Show one post
+router.get('/:id?', (req, res) =>{
+    let filter = '';
+    if(req.params.id) filter = ' WHERE id_post=' + parseInt(req.params.id);
+    execSQLQuery('SELECT * FROM post' + filter, res);
 });
 
-/* PATCH Atualiza os campos passados do posts. */
-router.patch('/:id', function(req, res, next) {
-    res.send('Rota posts!')
+//Delete one post
+router.delete('/:id', (req, res) =>{
+    execSQLQuery('DELETE FROM post WHERE id_post=' + parseInt(req.params.id), res);
 });
+
+//Insert one post
+router.post('/', (req, res) =>{
+    const description_post = req.body.description_post;
+
+    let created_at = req.body.created_at;
+
+    let updated_at = req.body.updated_at;
+
+    execSQLQuery(`INSERT INTO post(description_post, created_at, updated_at) VALUES('${description_post}', '${created_at}', '${updated_at}')`, res);
+});
+
+//Update one post
+router.patch('/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+    
+    const description_post = req.body.description_post;
+
+    let created_at = req.body.created_at;
+
+    let updated_at = req.body.updated_at;
+
+
+    execSQLQuery(`UPDATE post SET description_post = '${description_post}', created_at = '${created_at}', updated_at = '${updated_at}' WHERE id_post=${id}`, res);
+})
 
 module.exports = router;  
--- src/routes/role.js
+++ src/routes/role.js
@@ -1,15 +1,77 @@
 var express = require('express');
+const app = express();  
+
 var router = express.Router();
 
-/* GET Listagem role. */
+
+/* GET Listagem status_dream. */
+/*
 router.get('/', function(req, res, next) {
-    res.send('Rota role!')
+    res.send('Rota status_dream!')
 });
-
-/* GET Busca role por id. */
+*/
+/* GET Busca status_dream por id. */
+/*
 router.get('/:id', function(req, res, next) {
-    res.send('Rota role!')
+    res.send('Rota status_dream!')
 });
 
+*/
+
+
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+function execSQLQuery(sqlQry, res){
+    const connection = mysql.createConnection({
+        host     : 'localhost',
+        port     : 3306,
+        user     : 'root',
+        password : 'teste123',
+        database : 'SistemaBD'
+    });
+
+    connection.query(sqlQry, function(error, results, fields){
+        if(error) 
+            res.json(error);
+        else
+            res.json(results);
+        connection.end();
+        console.log('executou!');
+    });
+}
+
+//Show all roles
+router.get('/', (req, res) => {
+	execSQLQuery('SELECT * FROM role', res);
+});
+
+//Show one role
+router.get('/:id?', (req, res) =>{
+    let filter = '';
+    if(req.params.id) filter = ' WHERE id_role=' + parseInt(req.params.id);
+    execSQLQuery('SELECT * FROM role' + filter, res);
+});
+
+//Delete one role
+router.delete('/:id', (req, res) =>{
+    execSQLQuery('DELETE FROM role WHERE id_role=' + parseInt(req.params.id), res);
+});
+
+//Insert one role
+router.post('/', (req, res) =>{
+    const description_role = req.body.description_role;
+    execSQLQuery(`INSERT INTO role(description_role) VALUES('${description_role}')`, res);
+});
+
+//Update one role
+router.patch('/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+    const description_role = req.body.description_role;
+    execSQLQuery(`UPDATE role SET description_role='${description_role}' WHERE id_role=${id}`, res);
+})
 
 module.exports = router;  
