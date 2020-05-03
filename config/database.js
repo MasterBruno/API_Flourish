701f0f643e068a068dc4413400731174472bf31b
Bruno César <brunoc.15@hotmail.com>
2020-05-02 22:32:09 -0300

    Configuração inicial sequelize.

---
+++ config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'db_user',
+    password: 'db_1234',
+    database: 'db_flourish',
+    define: {
+        timestamps: true,
+        underscored: true,
+    }
+}
